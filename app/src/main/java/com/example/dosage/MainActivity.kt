ac224a7cdcd90d4e208d9950ed88cf5497f6a0a2
DESKTOP-P6ARO04\balsw <domi29@naver.com>
2023-03-09 16:22:21 +0900

    API 값 가져오기

--- app/src/main/java/com/example/dosage/MainActivity.kt
+++ app/src/main/java/com/example/dosage/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.dosage
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.example.dosage.databinding.ActivityMainBinding
+import org.json.JSONArray
 import org.json.JSONObject
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+
+var text = ""
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,75 +28,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+
+        // api 불러오기 위한 정보
+        val key = ""
+        val pageNo = "1"
+        val numOfRows = "3"
+        val type = "xml"
+        val site = "http://apis.data.go.kr/1471000/DayMaxDosgQyByIngdService/getDayMaxDosgQyByIngdInq?serviceKey="+
+                key+"&numOfRows="+numOfRows+"&pageNo="+pageNo+"&type="+type
+
         binding.searchBtn.setOnClickListener{
-            val thread = NetWorkThread()
+            val thread = Thread(NetWorkThread(site))
             thread.start()
             thread.join()
+            binding.testText.text = text
         }
 
     }
 
-    inner class NetWorkThread: Thread(){
+    inner class NetWorkThread(var url: String): Runnable{
+          @RequiresApi(Build.VERSION_CODES.N)
           override fun run(){
-              //serviceKey
-              val key = ""
+                try{
+                    val xml: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url)
+                    xml.documentElement.normalize()
 
-              //pageNo
-              val pageNo = "1"
+                    //찾고자 하는 데이터가 어느 노드 아래에 있는지 확인
+                    val list: NodeList = xml.getElementsByTagName("item")
 
-              //numOfRows
-              val numOfRows = "3"
+                    for(i in 0..list.length-1){
+                        val n: Node = list.item(i)
+                        if(n.getNodeType() == Node.ELEMENT_NODE){
+                            val elem = n as Element
+                            val map = mutableMapOf<String, String>()
 
-              //type
-              val type = "xml"
+                            for(j in 0..elem.attributes.length-1){
+                                map.putIfAbsent(elem.attributes.item(j).nodeName, elem.attributes.item(j).nodeValue)
+                            }
+                             text = "${elem.getElementsByTagName("DRUG_CPNT_KOR_NM").item(0).textContent}"
+                            break
+                        }
+                    }
 
-              // API 정보를 가지고 있는 주소
-              val site = "http://apis.data.go.kr/1471000/DayMaxDosgQyByIngdService/getDayMaxDosgQyByIngdInq?serviceKey="+
-                      key+"&numOfRows="+numOfRows+"&pageNo="+pageNo+"type="+type
-
-              val url = URL(site)
-              val conn = url.openConnection()
-              val input = conn.getInputStream()
-              val isr = InputStreamReader(input)
-              // br 라인 단위로 데이터 읽어옴
-              val br = BufferedReader(isr)
-
-              var str: String? = null
-              val buf = StringBuffer()
-
-              do{
-                  str = br.readLine()
-
-                  if(str!=null){
-                      buf.append(str)
-                  }
-              }while (str!=null)
-
-              val root = JSONObject(buf.toString())
-              val response = root.getJSONObject("response").getJSONObject("body").getJSONObject("items")
-              val item = response.getJSONArray("item")
-
-              //화면에 출력
-              runOnUiThread {
-                  //페이지 수 만큼 반복하여 데이터를 불러온다
-                  for(i in 0 until item.length()){
-                      val jObject = item.getJSONObject(i)
-                      if(binding.search.text.toString() == "${ JSON_Parse(jObject,"addr1")}"){
-                          searchKeyword(binding.search.text.toString())
-                      }
-                  }
-              }
-
-          }
-          // 함수를 통해 데이터를 불러온다.
-          fun JSON_Parse(obj:JSONObject, data : String): String {
-
-                // 원하는 정보를 불러와 리턴받고 없는 정보는 캐치하여 "없습니다."로 리턴받는다.
-                return try {
-                    obj.getString(data)
-                } catch (e: Exception) {
-                    "없습니다."
+                } catch (e: Exception){
+                    Log.d("MAIN", "오픈API"+e.toString())
                 }
+
           }
 
     }
